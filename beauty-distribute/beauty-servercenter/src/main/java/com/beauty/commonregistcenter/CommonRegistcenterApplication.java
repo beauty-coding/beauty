71d0239f7ed5b07ce897552ae4554e16d992b804
yufw <yufw@deepblueai.com>
2021-01-24 13:03:22 +0800

    -----append

--- beauty-distribute/beauty-servercenter/src/main/java/com/beauty/commonregistcenter/CommonRegistcenterApplication.java
+++ beauty-distribute/beauty-servercenter/src/main/java/com/beauty/commonregistcenter/CommonRegistcenterApplication.java
@@ -2,7 +2,12 @@ package com.beauty.commonregistcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author yufw
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class CommonRegistcenterApplication {
 
--- beauty-search/src/main/java/com/beauty/commonsearch/CommonSearchApplication.java
+++ beauty-search/src/main/java/com/beauty/commonsearch/CommonSearchApplication.java
@@ -3,6 +3,9 @@ package com.beauty.commonsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yufw
+ */
 @SpringBootApplication
 public class CommonSearchApplication {
 
